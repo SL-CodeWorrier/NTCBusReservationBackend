c9291aea5508a5528e9a7b8eb949d00a0d90be60
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-12-15 14:09:47 +0530

    Added commuter model

---
+++ src/Models/commuterModel.js
@@ -0,0 +1,16 @@
+const { timeStamp } = require("console");
+const mongoose = require("mongoose");
+const { type } = require("os");
+
+const commuterSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    addressNo: { type: String, required: true },
+    addressFirstLine: { type: String, required: true },
+    addressSecondLine: { type: String, required: false },
+    city: { type: String, required: true },
+    province: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true }, // Unique User reference
+}, { timestamps: true });
+
+module.exports = mongoose.model("Commuter", commuterSchema);
