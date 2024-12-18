865ebb44cd39dbd7da40ef1d06265ba12a8ccabf
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-12-18 23:25:58 +0530

    updated

--- src/Models/busOperatorModel.js
+++ src/Models/busOperatorModel.js
@@ -5,9 +5,9 @@ const { type } = require("os");
 
 // BusOperator schema definition
 const busOperatorSchema = new mongoose.Schema({
-    businessPhoneNumber: { type: String, required: true }, // Operator's business contact number
+    businessPhoneNumber: { type: String, required: true, unique: true }, // Operator's business contact number
     isWorkingProfile: { type: Boolean, required: true },  // Indicates if the profile is active
-    operatorNumber: { type: String, required: true },     // Unique identifier for the bus operator
+    operatorNumber: { type: String, required: true, unique: true },     // Unique identifier for the bus operator
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}, // Foreign Key to User
 }, { timestamps: true }); // Adds createdAt and updatedAt timestamps
 
--- src/Models/commuterModel.js
+++ src/Models/commuterModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { type } = require("os");
 
 const commuterSchema = new mongoose.Schema({
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     addressNo: { type: String, required: true },
