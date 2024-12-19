4478b09d082765c21aebcc842600db7d3ce548a2
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-12-19 20:35:35 +0530

    added bus and route availability routes files

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const authRoutes = require("./Routes/authRoute");
 const commuterRoutes = require("./Routes/commuterRoute");
 const busOperatorRoutes = require("./Routes/busOperatorRoute");
 const routeRoutes = require("./Routes/routeRoutes");
+const routeAvailabilityRoutes = require('./Routes/routeAvailabilityRoute');
+const busRoutes = require('./Routes/busRoute');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger'); // Import Swagger configuration
@@ -24,6 +26,8 @@ app.use("/api/v1/lk/auth", authRoutes);
 app.use("/api/v1/lk/commuter", commuterRoutes);
 app.use("/api/v1/lk/bus-operators", busOperatorRoutes);
 app.use("/api/v1/lk/routes", routeRoutes);
+app.use("/api/v1/lk/routes-availability", routeAvailabilityRoutes);
+app.use("/api/v1/lk/buses", busRoutes);
 
 const PORT = process.env.PORT || 7002;
 app.listen(PORT, () => {
